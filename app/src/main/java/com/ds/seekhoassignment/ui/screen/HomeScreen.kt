822237f26297fd0fe49354cff9b618687515d9f4
si-deepak-sahu <151026684+si-deepak-sahu@users.noreply.github.com>
2025-05-10 19:03:01 +0530

    AnimeCard UI improved according to requirement

--- app/src/main/java/com/ds/seekhoassignment/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/ds/seekhoassignment/ui/screen/HomeScreen.kt
@@ -8,15 +8,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,12 +29,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
@@ -76,26 +84,15 @@ fun ListDataCompose(
             contentPadding = PaddingValues(16.dp)
         ) {
             item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .padding(vertical = 25.dp),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        "Popular anime",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
+                Header("Popular Anime")
             }
 
             items(animeList.size) { index ->
                 val data = animeList[index]
                 AnimeCard(
                     data.title.orEmpty(),
-                    data.synopsis.orEmpty(),
+                    data.score.toString(),
+                    data.titles?.size ?: 0,
                     data.images?.jpg?.largeImageUrl.orEmpty()
                 ) {
                     navController.navigate("${NavRoutes.DETAILS_SCREEN}/${data.trailer?.youtubeId ?: ""}")
@@ -105,12 +102,30 @@ fun ListDataCompose(
     }
 }
 
+@Composable
+private fun Header(title: String) {
+    Row(
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(bottom = 15.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+    }
+}
 
 @Composable
-fun AnimeCard(name: String, description: String, image: String, click: () -> Unit) {
+fun AnimeCard(name: String, score: String, episodes: Int, image: String, click: () -> Unit) {
     Card(
         modifier = Modifier
-            .padding(horizontal = 10.dp, vertical = 5.dp)
+            .padding(vertical = 5.dp)
             .background(MaterialTheme.colorScheme.background)
             .fillMaxWidth()
             .wrapContentHeight()
@@ -128,30 +143,49 @@ fun AnimeCard(name: String, description: String, image: String, click: () -> Uni
                     placeholder = painterResource(id = android.R.drawable.ic_media_play),
                     error = painterResource(id = android.R.drawable.stat_notify_error)
                 ),
-                contentDescription = null,
+                contentDescription = "Poster image",
                 modifier = Modifier
                     .padding(8.dp)
-                    .height(120.dp)
-                    .width(100.dp),
-                contentScale = ContentScale.Fit,
+                    .size(80.dp)
+                    .clip(MaterialTheme.shapes.medium),
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center
             )
             Column(Modifier.padding(horizontal = 8.dp)) {
                 Text(
                     text = name,
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
                     color = MaterialTheme.colorScheme.onSurface,
                     maxLines = 1,
+                    fontSize = 16.sp,
                     overflow = TextOverflow.Ellipsis,
                     textAlign = TextAlign.Center,
                 )
 
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Rating",
+                        tint = Color(0xFFFFC107),
+                        modifier = Modifier.size(14.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = score,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                    )
+                }
                 Text(
-                    text = description,
+                    text = "Episodes: $episodes",
                     style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    maxLines = 4,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Start,
+                    textAlign = TextAlign.Center,
                 )
             }
         }
@@ -166,29 +200,14 @@ fun PreviewHomeScreen() {
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(vertical = 25.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    "Popular anime",
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
+            Header("Popular Anime ")
             AnimeCard(
-                "Sousou no Frieren",
-                "uring their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles",
+                "Sousa no Friend",
+                "9.3",
+                10,
                 "https://cdn.myanimelist.net/images/anime/1015/138006l.jpg"
             ) {}
-            AnimeCard(
-                "Sousou no Frieren",
-                "uring their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles",
-                "https://cdn.myanimelist.net/images/anime/1015/138006l.jpg"
-            ) {}
+
         }
     }
 }
